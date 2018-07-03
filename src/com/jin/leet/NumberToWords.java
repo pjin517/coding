3ada16b802a5de491432166f0ca9f2a3342c8967
pjin517 <38093421+pjin517@users.noreply.github.com>
2018-07-03 11:08:23 -0700

    better solution

--- src/com/jin/leet/NumberToWords.java
+++ src/com/jin/leet/NumberToWords.java
@@ -23,8 +23,45 @@ import java.util.HashMap;
  * Input: 1234567891
  * Output: "One Billion Two Hundred Thirty Four Million Five Hundred Sixty Seven Thousand Eight Hundred Ninety One"
  *
- * TODO:
+ * https://leetcode.com/problems/integer-to-english-words/discuss/70625/My-clean-Java-solution-very-easy-to-understand
+ *
  */
+
+
+/**
+ * private final String[] LESS_THAN_20 = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
+ * private final String[] TENS = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+ * private final String[] THOUSANDS = {"", "Thousand", "Million", "Billion"};
+ *
+ * public String numberToWords(int num) {
+ *     if (num == 0) return "Zero";
+ *
+ *     int i = 0;
+ *     String words = "";
+ *
+ *     while (num > 0) {
+ *         if (num % 1000 != 0)
+ *     	    words = helper(num % 1000) +THOUSANDS[i] + " " + words;
+ *     	num /= 1000;
+ *     	i++;
+ *     }
+ *
+ *     return words.trim();
+ * }
+ *
+ * private String helper(int num) {
+ *     if (num == 0)
+ *         return "";
+ *     else if (num < 20)
+ *         return LESS_THAN_20[num] + " ";
+ *     else if (num < 100)
+ *         return TENS[num / 10] + " " + helper(num % 10);
+ *     else
+ *         return LESS_THAN_20[num / 100] + " Hundred " + helper(num % 100);
+ * }
+ */
+
+
 public class NumberToWords {
     final int THOUSAND = 1000;
     final int MILLION = 1000000;
