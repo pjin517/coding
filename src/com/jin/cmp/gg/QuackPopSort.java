c08faca87d077272a7aeed22017e8eb4abada356
pj <jinpingv@gmail.com>
2018-08-16 15:39:21 -0700

    more

---
+++ src/com/jin/cmp/gg/QuackPopSort.java
@@ -0,0 +1,12 @@
+package com.jin.cmp.gg;
+
+/**
+ * 给定一个Quack class。基本上就是一个a stack or queue，已经按升序排好， .pop() will randomly pop from head or tail of this data structure. 要求返利用这个.pop()，返回降序排列的array。
+ *
+ * From <http://www.1point3acres.com/bbs/forum.php?mod=viewthread&tid=436592&ctid=84995>
+ *
+ *  ToDO:
+ */
+public class QuackPopSort {
+
+}
